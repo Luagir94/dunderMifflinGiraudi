d1127f891c78cae8cc8607b6048f78cb9ccb9ebf
Luciano Giraudi <lucianoagiraudi@gmail.com>
2021-07-06 18:36:05 -0300

    preentrega proyecto final

--- src/Pages/ItemCategory.js
+++ src/Pages/ItemCategory.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react';
+import {useParams} from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
@@ -23,7 +24,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 export default function ItemCategory() {
-    const {categoryId} = useParams();
+  const {categoryId} = useParams();
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
