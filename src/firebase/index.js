53e61bc5bd1bfcc8dd4d3c7855c9806be0e394cc
Luciano Giraudi <78420902+Luagir94@users.noreply.github.com>
2022-09-27 19:52:59 -0300

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,7 @@
 import firebase from "firebase/app";
 import 'firebase/firestore';
 
-const firebaseConfig ={
-    apiKey: "AIzaSyAgmQJ5VnrHkE4w7ADZo5Bb_b4RuQo7KgQ",
-    authDomain: "dundermifflin-859d3.firebaseapp.com",
-    projectId: "dundermifflin-859d3",
-    storageBucket: "dundermifflin-859d3.appspot.com",
-    messagingSenderId: "689828650571",
-    appId: "1:689828650571:web:66aace43e786ffede0b7ce",
-    measurementId: "G-4WDV6BYM21"
-  };
+
 
 const app = firebase.initializeApp(firebaseConfig)
 
